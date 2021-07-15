2397ca8385e96676cecb8e691d3a3a7dc8bdff9f
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-07-15 20:14:10 +0200

    Added fetching data from API

--- Cryptocurrency price listing app/ViewController.m
+++ Cryptocurrency price listing app/ViewController.m
@@ -8,15 +8,32 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property NSMutableArray *tickers;
 @end
 
 @implementation ViewController
 
+@synthesize tickers;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self fetchDataFromApi];
+    
 }
 
+- (void)fetchDataFromApi {
+    NSURL *url = [NSURL URLWithString:@"https://api.coinlore.net/api/tickers/?start=100&limit=10"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+        if (data == nil) { return; }
+        
+        self.tickers = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+        NSLog(@"%@",self.tickers);
+    }];
+    
+    [task resume];
+}
 
 @end
